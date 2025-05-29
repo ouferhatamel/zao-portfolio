e8b665ac3d220b59eafd09fecf7ab296118ce588
Amel Ouferhat <ouferhatamel@gmail.com>
2025-05-29 17:12:42 -0400

    Add business card component

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,13 @@
 import './Contact.css'
 import Izao from "../../components/Izao/Izao";
 import ContactMe from '../../components/ContactMe/ContactMe';
+import BizCard from '../../components/BizCard/BizCard';
 
 export default function Contact() {
   return (
     <div className="contact pageStyle">
       <div className="left">
-        <Izao />
+        <BizCard />
       </div>
       <div className="right">
         <ContactMe />
